a8a40ac7438bb72528f643c75f5551a61e7437e7
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2015-10-14 12:46:20 +0200

    Issue2 : "A propos" page add more "debug" informations - step2
    * Add geolocalisation tests
    * Rename  "hasMed" ---> "hasUserMedia"

--- www/js/controllers/AProposController.js
+++ www/js/controllers/AProposController.js
@@ -16,7 +16,7 @@ angular.module('artmobilis').controller('AProposController',
         $scope.$on("$ionicView.beforeLeave", function (e) {
 
         });
-        var hasMed = function hasMed() {
+        var hasUserMedia = function hasUserMedia() {
 
             navigator.getUserMedia = (navigator.getUserMedia ||
                navigator.webkitGetUserMedia ||
@@ -24,31 +24,48 @@ angular.module('artmobilis').controller('AProposController',
                navigator.msGetUserMedia);
 
             if (window.hasUserMedia()) {
-                $scope.usermedia = "Browser supports getUserMedia.";
+                $scope.usermedia = "getUserMedia supported by the browser.";
             } else {
-                $scope.usermedia = "Browser does not support getUserMedia.";
+                $scope.usermedia = "getUserMedia is not supported by the browser</em>";
             }
+        };
+
+        var hasGeolocation = function hasGeolocation() {
+
+          if (navigator.geolocation) {
+              $scope.gps = "Geolocation supported by the browser.";
+              navigator.geolocation.getCurrentPosition(showPosition, showGeolocError);
+          } else {
+              $scope.gps = "Geolocation is not supported by the browser.";
+          }
 
         };
 
-        var hasGPS = function hasGPS() {
+        function showPosition(position) {
+            $scope.gps += "Lat:" + position.coords.latitude +
+            "Long:" + position.coords.longitude;
+        }
 
-            navigator.getUserMedia = (navigator.getUserMedia ||
-               navigator.webkitGetUserMedia ||
-               navigator.mozGetUserMedia ||
-               navigator.msGetUserMedia);
-
-            if (window.hasUserMedia()) {
-                $scope.gps = "Browser supports getUserMedia.";
-            } else {
-                $scope.gps = "Browser does not support getUserMedia.";
-            }
-
-        };
+        function showGeolocError(error) {
+          switch(error.code) {
+            case error.PERMISSION_DENIED:
+                $scope.gps += " User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                $scope.gps += " Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                $scope.gps += " The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                $scope.gps += " An unknown error occurred."
+                break;
+          }
+}
 
         $scope.$on('$ionicView.enter', function (e) {
-            hasMed();
-            hasGPS();
+            hasUserMedia();
+            hasGeolocation();
 
         });
 
