e83404a02d5daae64f3010e9bced10913506c691
Bruce LANE <bruce@batchass.fr>
2015-10-27 18:49:05 +0100

    parcours essai

--- www/js/controllers/MapController.js
+++ www/js/controllers/MapController.js
@@ -44,6 +44,25 @@ angular.module('artmobilis').controller('MapController',
                 $scope.show(i);
             };
 
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJson(myLines, {
+    style: myStyle
+}).addTo($scope.map);
+
+
             $scope.goTo(0); 
 
             
