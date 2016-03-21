60fdbfaa4ebe8a8ac7614ac7db6f8fedc79462a9
calemany <calemany@student.42.fr>
2016-03-21 16:49:36 +0100

    Remove false dependencies

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 
 angular.module('starter', ['ionic', 'journey'])
 
-.run(['$ionicPlatform', 'DataManagerSvc', 'LoadingSvc', function($ionicPlatform, DataManagerSvc, LoadingSvc) {
+.run(['$ionicPlatform', function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
